f5ef4f34e6c31e4aedf76914fb42a33b8048e602
Lucas Silva <lucas@brainyresolution.com>
2021-07-08 21:44:49 -0300

    chore(users) replaced hardcoded url with reverse function

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,6 +1,7 @@
 from django.http import response
 from rest_framework.test import APITestCase
 from users.models import User
+from django.urls import reverse
 
 
 class UserTesting(APITestCase):
@@ -11,7 +12,7 @@ class UserTesting(APITestCase):
                      "password": "123", "date_of_birth": "1996-03-19"}
 
         user_data.update(data)
-        response = self.client.post("/api/users/", data=user_data)
+        response = self.client.post(reverse("users-list"), data=user_data)
         return response
 
     def test_registration(self):
@@ -26,7 +27,7 @@ class UserTesting(APITestCase):
         old_password = user.password
 
         response = self.client.patch(
-            f"/api/users/{user.id}/", data={"password": "1235"})
+            reverse("users-detail", kwargs={"pk": user.id}), data={"password": "1235"})
         self.assertEqual(response.status_code, 200)
 
         user = User.objects.all().first()
@@ -39,7 +40,7 @@ class UserTesting(APITestCase):
         self.post_user()
 
         response = self.client.post(
-            "/api/token/", data={"email": "lucas@gmail.com", "password": "123"})
+            reverse('token'), data={"email": "lucas@gmail.com", "password": "123"})
 
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.json().get("access", None), None)
